c79ccc3ef7a2d173744ce4400a5f0244879ff9e8
Karollainny Teles <karollainnyteles@hotmail.com>
2021-09-06 15:42:06 -0300

    Operações no banco com EF

--- ProjetoTesteMVC5/Controllers/AlunoController.cs
+++ ProjetoTesteMVC5/Controllers/AlunoController.cs
@@ -1,6 +1,8 @@
-﻿using ProjetoTesteMVC5.Models;
+﻿using ProjetoTesteMVC5.Data;
+using ProjetoTesteMVC5.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +11,9 @@ namespace ProjetoTesteMVC5.Controllers
 {
     public class AlunoController : Controller
     {
+
+        private AppDbContext context = new AppDbContext();
+
         [HttpGet]
         [Route("novo-aluno")]
         public ActionResult NovoAluno()
@@ -24,12 +29,57 @@ namespace ProjetoTesteMVC5.Controllers
         {
             if (!ModelState.IsValid)
             {
-                return View(aluno);
+                return View(aluno); 
             }
+
+            aluno.DataMatricula = DateTime.Now;
+            context.Alunos.Add(aluno);
+            context.SaveChanges();
 
             return View(aluno);
 
         }
-           
+
+        [HttpGet]
+        [Route("obter-alunos")]
+        public ActionResult ObterAlunos()
+        {
+            var alunos = context.Alunos.ToList();
+            return View("NovoAluno",alunos.FirstOrDefault());
+
+        }
+
+        [HttpGet]
+        [Route("editar-alunos")]
+        public ActionResult EditarAluno()
+        {
+            var aluno = context.Alunos.FirstOrDefault(a => a.Nome == "karol teles");
+
+            aluno.Nome = "Karollainny Teles";
+            var entry = context.Entry(aluno);
+            context.Set<Aluno>().Attach(aluno);
+            entry.State = EntityState.Modified;
+
+            context.SaveChanges();
+
+            var alunoModificado = context.Alunos.Find(aluno.Id);
+            return View("NovoAluno", alunoModificado);
+
+        }
+
+        [HttpGet]
+        [Route("excluir-alunos")]
+        public ActionResult ExcluirAluno()
+        {
+            var aluno = context.Alunos.FirstOrDefault(a => a.Nome == "teste");
+
+            context.Alunos.Remove(aluno);
+            context.SaveChanges();
+
+            var alunos = context.Alunos.ToList();
+            return View("NovoAluno", alunos.FirstOrDefault());
+
+        }
+
     }
-}
+}   
